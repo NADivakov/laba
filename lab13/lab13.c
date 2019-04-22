2048f37a740ece7dc23418df91f0aba3c83db86c
NADivakov <43946859+NADivakov@users.noreply.github.com>
2019-04-22 16:11:41 +0300

    Update lab13.c

--- lab13/lab13.c
+++ lab13/lab13.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+#define M 6
 
+int main()
+{
+int a[N][M], i, j, k, id, max;
+srand(time(NULL));
+for (i=0; i<N; i++){
+    for (j=0; j<M; j++){
+        a[i][j] = rand()%50 - 25;
+        printf("%4d", a[i][j]);
+    }
+    printf("\n");
+}
+printf("\n");
+
+k = M-1;
+while (k > 0){
+    id = 0;
+    for (j=1; j<=k; j++)
+    if (a[0][id])
+    id = j;
+    for (i=0; i<N; i++){
+        max = a[i][id];
+        a[i][id] = a[i][k];
+        a[i][k] = max;
+    }
+    k-=1;
+}
+
+for (i=0; i<N; i++){
+for (j=0; j<M; j++){
+    printf("%4d",a[i][j]);
+}
+printf("\n");
+}
+}
